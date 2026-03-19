be2fb41819b6e37b91d6b174e8393667869d470c
anaserkinov <anaserkinjonov@gmail.com>
2026-03-19 21:54:44 +0500

    update server location

--- deploy_compose.sh
+++ deploy_compose.sh
@@ -2,12 +2,12 @@
 
 (cd composeApp/build/dist/wasmJs && rm -f install.zip && zip -r install.zip productionExecutable)
 
-scp composeApp/build/dist/wasmJs/install.zip root@188.245.102.242:/var/www/portfolio/
+scp composeApp/build/dist/wasmJs/install.zip root@188.245.102.242:/var/www/portfolio_compose/
 
 ssh root@188.245.102.242 '
 cd /var/www/ &&
-find portfolio/* ! -name "install.zip" -exec rm -rf {} + &&
-cd portfolio &&
+find portfolio_compose/* ! -name "install.zip" -exec rm -rf {} + &&
+cd portfolio_compose &&
 unzip -o install.zip -d . &&
 mv productionExecutable/* . &&
 rm -rf productionExecutable &&
